700431511510ef2e1a07293e9ffa8766c3be52c3
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-03-23 15:45:08 +0530

    install react and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <h1 class="text-9xl font-bold underline">
+      Hamid ibrahim
+  </h1>
+    </div>
+  )
+}
+
+export default App
