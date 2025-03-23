3c810b5dde39de2a2176ab3e5aa7d4439c57fb36
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-03-23 17:52:39 +0530

    db connected

--- api/server.js
+++ api/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import { error } from 'console';
 
 const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log('DB connected'))
+.catch((error)=>console.error(error))
 
 app.listen(3000, ()=> console.log('server running on',3000));
