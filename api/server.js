5307639fe022b99ed54fbd44725f7d4d53853968
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-03-24 11:05:39 +0530

    added test api

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-import { error } from 'console';
+import userRoute from './routes/user.routes.js'
+
 
 const app = express()
 dotenv.config()
@@ -9,5 +10,7 @@ dotenv.config()
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log('DB connected'))
 .catch((error)=>console.error(error))
+
+app.use('/api/user',userRoute)
 
 app.listen(3000, ()=> console.log('server running on',3000));
