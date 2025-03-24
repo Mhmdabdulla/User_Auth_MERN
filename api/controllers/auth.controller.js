1b0fb8e5ac5d822ece6165e868ff17404f766bae
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-03-24 14:15:21 +0530

    added error handling functions

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model.js';
 
-export const signup = async (req,res)=>{
+export const signup = async (req,res,next)=>{
     try {
         const {username,email,password} = req.body;
         const hashedPass = bcryptjs.hashSync(password,10);
@@ -9,7 +9,6 @@ export const signup = async (req,res)=>{
         await newUser.save();
         res.status(201).json({message:'User created successfully'})
     } catch (error) {
-        console.log(error.message)
-        res.status(500).json(error.message)
+        next(error)
     }
 }
--- api/server.js
+++ api/server.js
@@ -13,7 +13,17 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log('DB connected'))
 .catch((error)=>console.error(error))
 
+app.listen(3000, ()=> console.log('server running on',3000));
+
 app.use('/api/user',userRoute);
 app.use('/api/auth',authRouter)
 
-app.listen(3000, ()=> console.log('server running on',3000));
+app.use((err,req,res,next)=>{
+    const statuscode = err.statuscode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statuscode).json({
+        success:false,
+        message,
+        statuscode
+    })
+})
