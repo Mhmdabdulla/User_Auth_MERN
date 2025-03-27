9bfc5ffa201e0bb3f2de89bf4ad7cee7c9de87c6
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-03-27 23:15:40 +0530

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,26 @@
 import React, { useRef, useState,useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { ToastContainer, toast } from "react-toastify";
+import { useDispatch } from 'react-redux';
+ import {
+   updateUserStart,
+   updateUserSuccess,
+   updateUserFailure,
+ } from '../redux/user/userSlice';
 
 const Profile = () => {
-  const {currentUser} = useSelector(state=>state.user)
-
+  const {currentUser,loading,error} = useSelector(state=>state.user)
+  const dispatch = useDispatch()
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
 
   useEffect(() => {
     if (image) {
+      console.log('from useeffect',error)
       handleFileUpload(image);
     }
   }, [image]);
@@ -33,6 +41,7 @@ const Profile = () => {
         body: formData,
       });
       const data = await response.json();
+      console.log(data)
       if (data.secure_url) {
         setFormData((prev) => ({ ...prev, profilePicture: data.secure_url }));
         setImageError(false);
@@ -45,11 +54,38 @@ const Profile = () => {
     }
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data)
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-    <form className='flex flex-col gap-4'>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
     <input
            type='file'
            ref={fileRef}
@@ -69,6 +105,7 @@ const Profile = () => {
         type='text'
         id='username'
         placeholder='Username'
+        onChange={handleChange}
         className='bg-slate-100 rounded-lg p-3'
       />
       <input
@@ -77,22 +114,28 @@ const Profile = () => {
         id='email'
         placeholder='Email'
         className='bg-slate-100 rounded-lg p-3'
+        onChange={handleChange}
       />
       <input
         type='password'
         id='password'
         placeholder='Password'
         className='bg-slate-100 rounded-lg p-3'
+        onChange={handleChange}
       />
       
       <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-           update
+           {loading ? 'loading...' : 'Update'}
          </button>
     </form>
     <div className='flex justify-between mt-5'>
       <span className='text-red-700 cursor-pointer'>Delete Account</span>
       <span className='text-red-700 cursor-pointer'>Sign out</span>
     </div>
+    <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+       <p className='text-green-700 mt-5'>
+         {updateSuccess && 'User is updated successfully!'}
+       </p>
   </div>
   )
 }
